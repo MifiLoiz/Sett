1c3d25e31ae65e4dc218a427890fceb8d42252d5
MifiLoiz <mifiloiz08@gmail.com>
2025-06-09 14:42:28 +0200

    Deck - eerste poging om een deck te genereren

---
+++ Create_Deck.py
@@ -0,0 +1,24 @@
+## Create a deck and table 
+# first draft 
+
+""" 
+For now, I just want it to create all possible cards and randomly pick 12 of these in order to create a table. 
+
+"""
+
+# green     red     purple
+# 1         2       3 
+# empty     striped filled
+# diamond   oval    squigle
+
+# Imports
+
+from Create_class2 import Card
+
+Card(1,2,2,1)
+
+
+class Deck: 
+    def __init__(self, colors = 3, quantities = 3, fillings = 3, shapes = 3):
+        pass
+
