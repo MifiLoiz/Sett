01f0b7017c0d01d36be82d3a1bbac13849392392
MifiLoiz <mifiloiz08@gmail.com>
2025-06-11 20:01:48 +0200

    some extra info

--- set_game_v1.py
+++ set_game_v1.py
@@ -5,12 +5,14 @@ from constants import *
 
 pygame.init()
 
-clock = pygame.time.Clock() 
+clock = pygame.time.Clock()     # of: pygame.time.get_ticks()
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 
 pygame.display.set_caption("Set")
 gameDisplay.fill(background_col)
+
+pygame.display.flip()
 
 exit = False
 
@@ -24,8 +26,17 @@ while not exit:
 """
 Source: 
     https://github.com/Mozes721/BlackJack
-    
+
 Code to get images: 
     show_card = pygame.image.load('kaarten/' + self.dealer.card_img[1] + '.png').convert()
+
+Clock info: 
+    https://www.geeksforgeeks.org/python-display-images-with-pygame/
+    https://nimbusintelligence.com/2023/05/gol-1-understanding-pygame/ 
+    
+Display your work on the screen: 
+    pygame.display.flip()
+
+    
 
 """
